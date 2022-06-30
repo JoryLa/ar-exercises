f43f0a9e11cb3b77ad9ea9f7d72810c43e29f01a
JorLo <jory.laurin@gmail.com>
2022-06-29 20:17:35 -0700

    adds to employees table and connects to stores table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employees < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Rob", last_name: "Bob", hourly_rate: 14)
+@store2.employees.create(first_name: "Larry", last_name: "Lobster", hourly_rate: 100)
