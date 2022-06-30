edd825bc285289d3b8a46c51e749f6360f9fa14d
JorLo <jory.laurin@gmail.com>
2022-06-29 20:48:59 -0700

    validations dont even work

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,12 +11,15 @@ puts "----------"
 # Your code goes here ...
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 2 }
 end
 
 class Employees < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, :last_name, :store_id, presence: true
+  # validates :hourly_rate, numericality: { in: 40..200 } doesn't work
 end
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Rob", last_name: "Bob", hourly_rate: 14)
+@store1.employees.create(first_name: "Rob", last_name: "Bob", hourly_rate: 0)
 @store2.employees.create(first_name: "Larry", last_name: "Lobster", hourly_rate: 100)
